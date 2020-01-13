e0f2e277ec8f65d8dbbd25b322fc9ec22c870fb7
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-01-13 05:10:31 -0500

    Create my-join.js and README.md
    Initial commit

---
+++ my-join.js
@@ -0,0 +1,14 @@
+function myJoin(array, separator) {
+    var string = ''
+    if(separator===undefined) {
+        separator = ','
+    }
+    for(let i=0; i<array.length; i++) {
+        if(array[i]===undefined || array[i]===null) string += '' + separator
+        else if(i !== array.length-1) string += array[i] + separator
+        else string += array[i]
+    }
+    return string;
+}
+
+console.log(myJoin(['hello', undefined, 'world'], '-'))
